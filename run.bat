e34564682e3c9e0b000183556483df899adea364
Quant Guy <148646431+KovachTechnologies@users.noreply.github.com>
2024-03-13 09:55:47 -0400

    adding scripts
    adding scripts to run miner on windows, linux and mac

--- run.bat
+++ run.bat
@@ -1,1 +1,11 @@
+REM
+REM name	: run.bat
+REM
+REM purpose	: run monero miner
+REM
+REM usage	: run.bat
+REM
+REM description	:
+REM
+
 xmr-stak-rx -o pool.supportxmr.com:8080 -u 4A4tgkWPxYiAzwUmMDzrXkZ1p5a8aacmSdqniMYTgQN7FX9wajLDvtxDHp7tXJsJHANAcadruP8Cmgx4qgAcSVP84iTaPMA --currency monero -p "windows computer"
--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+###########################################################################
+#
+# name		: run.sh
+#
+# purpose	: run monero miner
+#
+# usage		: ./run.sh
+#
+# description	:
+#
+###########################################################################
+
 ./xmrig --donate-level 0 -o pool.supportxmr.com:443 -u 4A4tgkWPxYiAzwUmMDzrXkZ1p5a8aacmSdqniMYTgQN7FX9wajLDvtxDHp7tXJsJHANAcadruP8Cmgx4qgAcSVP84iTaPMA -k --tls -p "mac/linux computer"
