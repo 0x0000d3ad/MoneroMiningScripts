64d4f6a23caa38d438df3225384518b6beda4e7c
Quant Guy <148646431+KovachTechnologies@users.noreply.github.com>
2024-03-13 20:29:16 -0400

    Update run_rpi.sh
    adding cpuminer since xmrig segfaulted

--- run_rpi.sh
+++ run_rpi.sh
@@ -12,4 +12,5 @@
 #
 ###########################################################################
 
-./xmrig --donate-level 0 -o pool.supportxmr.com:443 -u 4A4tgkWPxYiAzwUmMDzrXkZ1p5a8aacmSdqniMYTgQN7FX9wajLDvtxDHp7tXJsJHANAcadruP8Cmgx4qgAcSVP84iTaPMA -k --tls -p `hostname`
+#./xmrig --donate-level 0 -o pool.supportxmr.com:443 -u 4A4tgkWPxYiAzwUmMDzrXkZ1p5a8aacmSdqniMYTgQN7FX9wajLDvtxDHp7tXJsJHANAcadruP8Cmgx4qgAcSVP84iTaPMA -k --tls -p `hostname`
+./cpuminer -a cryptonight -o stratum+tcp://pool.supportxmr.com:8080 -u 4A4tgkWPxYiAzwUmMDzrXkZ1p5a8aacmSdqniMYTgQN7FX9wajLDvtxDHp7tXJsJHANAcadruP8Cmgx4qgAcSVP84iTaPMA -p node005
