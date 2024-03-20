d991109b4c52748be039fe3e5dd6112df083cc62
Quant Guy <148646431+KovachTechnologies@users.noreply.github.com>
2024-03-20 16:53:23 -0400

    Update run.sh
    updated run.sh for cryptonight algo that works

--- raspberry_pi/run.sh
+++ raspberry_pi/run.sh
@@ -12,5 +12,6 @@
 #
 ###########################################################################
 
-#./xmrig --donate-level 0 -o pool.supportxmr.com:443 -u 4A4tgkWPxYiAzwUmMDzrXkZ1p5a8aacmSdqniMYTgQN7FX9wajLDvtxDHp7tXJsJHANAcadruP8Cmgx4qgAcSVP84iTaPMA -k --tls -p `hostname`
-./cpuminer -a cryptonight -o stratum+tcp://pool.supportxmr.com:8080 -u 4A4tgkWPxYiAzwUmMDzrXkZ1p5a8aacmSdqniMYTgQN7FX9wajLDvtxDHp7tXJsJHANAcadruP8Cmgx4qgAcSVP84iTaPMA -p node005
+cd /home/daniel/cpuminer-multi
+dhclient eth0
+./cpuminer -a cryptonight -o stratum+tcp://cryptonight_upx.na.mine.zergpool.com:4457 -u DSvMzJQvThSCZwKwLBpiLuHSK6ce7LaESj -p c=DOGE,mc=UPX,ID=$HOSTNAME
