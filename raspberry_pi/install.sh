70c1139eb27c053e0a3e0cf1ac76992ebbf139a7
Quant Guy <148646431+KovachTechnologies@users.noreply.github.com>
2024-03-20 14:03:33 -0400

    Update install.sh
    adding net-tools (optional but useful)

--- raspberry_pi/install.sh
+++ raspberry_pi/install.sh
@@ -1,6 +1,6 @@
 cd ~/
 apt-get update && apt-get upgrade -y
-apt install git automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev build-essential -y
+apt install git automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev build-essential net-tools -y
 cd ~/
 git clone https://0x0000d3ad/cpuminer-multi
 cd cpuminer-multi
