ae52c00f905e6ec30ac2e58472ec496ed619bf37
Quant Guy <148646431+KovachTechnologies@users.noreply.github.com>
2024-03-20 14:01:44 -0400

    Update install.sh
    full install script for raspberry pi (run as root)

--- raspberry_pi/install.sh
+++ raspberry_pi/install.sh
@@ -1,7 +1,10 @@
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install git automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev build-essential -y
+cd ~/
+apt-get update && apt-get upgrade -y
+apt install git automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev build-essential -y
+cd ~/
 git clone https://0x0000d3ad/cpuminer-multi
 cd cpuminer-multi
-sudo ./autogen.sh
-sudo ./configure
-sudo ./build.sh
+./autogen.sh
+./configure
+./build.sh
+./run.sh
